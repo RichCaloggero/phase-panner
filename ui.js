b2364fda737d7c649446f091ced66fe7b48b917e
Rich Caloggero <rjc@mit.edu>
2018-11-16 14:05:23 -0500

    * automation works (very cool)!!
    - not well tested

--- ui.js
+++ ui.js
@@ -3,16 +3,20 @@ const element = elementOrId instanceof HTMLElement? elementOrId
 : document.getElementById(elementOrId);
 if (!element) return undefined;
 
-if (element.getAttribute("type") === "checkbox") return element.checked;
-else if (element.type === "text") return element.value;
+if (element.getAttribute("type") === "checkbox") {
+return element.checked;
+} else if (element.type === "text") {
+return element.value;
 
+} else {
 const value = element.value;
 return Number.isNaN(Number(value))?
 value
 : Number(value);
+} // if
 } // getValue
 
-function setValue (element, noChangeEvent) {
+function setValue (element, value, noChangeEvent) {
 element.value = value;
 if (noChangeEvent) return;
 else signal(element);
@@ -20,7 +24,7 @@ else signal(element);
 
 
 function signal (element, eventName = "change") {
-console.log(`signal: ${element}`);
+console.log("signal: ", element, eventName);
 
 element.dispatchEvent(new CustomEvent(eventName, {bubbles: true}));
 } // signal
@@ -32,13 +36,13 @@ return id? document.getElementById(id)
 } // ui
 
 function enumerateNumericControls () {
-enumerateUiControls()
+return enumerateUiControls ()
 .filter(x => x.matches("input[type='range'], input[type='number']"));
 } // enumerateNumericControls
 
 
 function enumerateUiControls () {
-return Array.from(document.querySelectorAll("#controls input"));
+return Array.from(document.querySelectorAll("#controls input, #controls select"));
 } // enumerateUiControls
 
 function getControlIds () {

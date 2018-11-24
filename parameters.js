075b8c14f9cf62e7cdb4bbe5d3ff4b8cb9842562
Rich Caloggero <rjc@mit.edu>
2018-11-23 21:21:39 -0500

    * have to stop....

--- parameters.js
+++ parameters.js
@@ -1,34 +1,12 @@
 const defaultParameter = {
-name: "",
+name: "", list: true,
 type: "", value: null,
-min: 0, max: 1,
+value: 0, min: 0, max: 1, step: 0.1,
 ui: null,
 updater: function (value) {console.log(`Parameter.updater: receiving value ${value}`);},
 automator: null,
 }; // defaultParameter
 
-const parameterData = [{
-name: "mix",
-}, {
-name: "xtcMix",
-}, {
-name: "frequency",
-value: 500, min: 20, max: 10000, step: 20
-}, {
-name: "q",
-value: 1, max: 10
-}, {
-name: "feedback",
-min: -1,
-}, {
-name: "delay"
-}, {
-name: "filterType", type: "select",
-values: ["lowpass", "highpass", "bandpass",
-"lowshelf", "highshelf",
-"peaking", "notch",
-"allpass"]
-}]; // parameterData
 
 
 
@@ -44,7 +22,13 @@ return parameters;
 function createUiControl (data) {
 if (!data.type && (data.min || data.max || data.step)) {
 element = document.createElement("input");
-element.type = data.type || "range";
+Object.assign(element, {
+type: data.type || "range",
+min: data.min,
+max: data.max,
+step: data.step,
+value: data.value
+}); // assign
 
 } else if (data.type === "select" || data.values instanceof Array) {
 element = document.createElement("select");

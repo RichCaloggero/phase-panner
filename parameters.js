6267424c4d3d12afea40b89bb4eb8d99b596419f
Rich Caloggero <rjc@mit.edu>
2018-11-25 01:09:00 -0500

    * added reverb.html
    cool!

--- parameters.js
+++ parameters.js
@@ -1,6 +1,6 @@
 const defaultParameter = {
 name: "", list: true,
-type: "", value: null,
+type: "range", value: null,
 value: 0, min: 0, max: 1, step: 0.1,
 selectedIndex: 0,
 ui: null,
@@ -21,20 +21,21 @@ return parameters;
 } // createParameterMap
 
 function createUiControl (data) {
-if (!data.type && (data.min || data.max || data.step)) {
+let element;
+if (data.type === "select" || data.options instanceof Array) {
+element = document.createElement("select");
+populateSelector(element, data.options);
+element.selectedIndex = data.selectedIndex;
+
+} else {
 element = document.createElement("input");
 Object.assign(element, {
-type: data.type || "range",
+type: data.type,
 min: data.min,
 max: data.max,
 step: data.step,
 value: data.value
 }); // assign
-
-} else if (data.type === "select" || data.options instanceof Array) {
-element = document.createElement("select");
-populateSelector(element, data.options);
-element.selectedIndex = data.selectedIndex;
 } // if
 
 const label = document.createElement("label");
